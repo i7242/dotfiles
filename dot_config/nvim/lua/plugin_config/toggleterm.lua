d7f69c0f56de3f0b0ac8483a595ade07795f4b9f
i7242 <xingyuyan@outlook.com>
2023-01-16 12:53:36 -0500

    add key map for toggleterm send to term

--- dot_config/nvim/lua/plugin_config/toggleterm.lua
+++ dot_config/nvim/lua/plugin_config/toggleterm.lua
@@ -2,3 +2,9 @@ require("toggleterm").setup{
   open_mapping = [[<c-\>]],
   direction = 'float'
 }
+
+local options = {noremap = true}
+
+-- Ctrl+e will send visual selected code to default terminal
+-- this can be line selection or block selection
+vim.api.nvim_set_keymap('v', '<C-e>', ':ToggleTermSendVisualSelection<CR>', options)
