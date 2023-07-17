090d4f3952ae3c8fd0e09dea555067cf33e0f749
i7242 <xingyuyan@outlook.com>
2023-07-16 21:05:19 -0400

    fix neovim Packer install

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,6 +5,15 @@
     - top level must be "lua", same level as this "init.lua"
 --]]
 
+-- Install Packer before load any other plugins & their configs
+local execute = vim.api.nvim_command
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
 -- Basic editor settings, won't be a lot
 require("basics")
 
