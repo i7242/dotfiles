7cb9698c41a0a30e3669832544ccd00bddbee113
i7242 <xingyuyan@outlook.com>
2025-05-25 10:32:48 -0400

    update vim config

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
   { "neanias/everforest-nvim" },
+  { "folke/tokyonight.nvim" },
+  { "EdenEast/nightfox.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "duskfox",
     },
   },
 }
